484606ccbaa15c99b145b930ed9bb0c57aeaf88e
MostafaHisham <mostafa.hisham@digi2.com>
2019-07-18 16:13:17 +0200

    handle invalid or empty email field while creating user model

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,11 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
+
+        #handling unpassed email address
+        if not email:
+            raise ValueError('Users must have an email address')
+
         user= self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
